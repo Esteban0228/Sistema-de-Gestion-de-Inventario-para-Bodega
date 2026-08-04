eba5eba4de723b5d7a5b4f6e65889883f0cb5d6d
Esteban0228 <espinosaclarosdanielesteban@gmail.com>
2026-08-03 22:21:15 -0500

    se añadio un archivo del grupo 10

---
+++ gestiondeinventarioparabodega/src/main/java/com/mycompany/gestiondeinventarioparabodega/Gestiondeinventarioparabodega.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestiondeinventarioparabodega;
+
+/**
+ *
+ * @author intel
+ */
+public class Gestiondeinventarioparabodega {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
